47f1fc0b7d07cfc0163e543e5f3dd1eca62eba0c
Nicolas Rossi <nrossi@gmail.com>
2013-04-01 15:10:51 -0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@
 #  updated_at :datetime         not null
 #
 
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base 
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
